871a4d6093010392710cdc359b032ce066699821
Mitch-Winchester <mitch.winchester91@outlook.com>
2024-10-04 15:19:45 -0400

    Update farm page to use ValTableLayout and ValLayout

--- src/components/val_table_layout.js
+++ src/components/val_table_layout.js
@@ -11,28 +11,41 @@ const ValTableLayout = ({
     filterFunction,
     data,
     headers,
-    imgBasePath
+    imgBasePath,
+    showSearch = true,
+    contentFlag = 'content',
+    contentNames = headers
 }) => {
+    // get first column name
+    let firstKey = contentNames[0];
+
     // Handle user input for the search
     const inputChange = (e) => {
-        setFilter(e.target.value.toLowerCase());
+        if (setFilter) {
+            setFilter(e.target.value.toLowerCase());
+        }
     };
 
     // Filter table items based on passed in filter function
-    const filteredItems = data.flatMap(node =>
-        node.content.filter(item => filterFunction(item, filter))
+    const filteredItems = filterFunction && filter !== undefined
+        ? data.flatMap(node =>
+            node[contentFlag].filter(item => filterFunction(item, filter))
+          )
+        : data.flatMap(node => node[contentFlag] // return all items if no filter
     );
 
     return (
         <>
-            <div className={searchContainer}>
-                <input 
-                    type="text" 
-                    id="searchBar" 
-                    placeholder="Search..." 
-                    onChange={inputChange}
-                />
-            </div>
+            {showSearch && setFilter && (
+                <div className={searchContainer}>
+                    <input 
+                        type="text" 
+                        id="searchBar" 
+                        placeholder="Search..." 
+                        onChange={inputChange}
+                    />
+                </div>
+            )}
             <div className={tableDiv}>
                 <table className={table}>
                     <thead>
@@ -45,17 +58,17 @@ const ValTableLayout = ({
                     </thead>
                     <tbody>
                         {filteredItems.map((item, index) => {
-                                let imagePath = `${imgBasePath}/${item.Item.replaceAll(' ', '_')}.png`;
+                                let imagePath = `${imgBasePath}/${item[firstKey].replaceAll(' ', '_')}.png`;
                             
                                 return (
                                     <tr key={index}>
                                         <td>
                                             <img
                                                 src={imagePath}
-                                                alt={item.Item}
+                                                alt={item[0]}
                                             />
                                         </td>
-                                        {headers.map(column => (
+                                        {contentNames.map(column => (
                                             <td>{item[column]}</td>
                                         ))}
                                     </tr>
--- src/pages/farm.js
+++ src/pages/farm.js
@@ -1,135 +1,51 @@
 import * as React from 'react'
-import { navigate, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Seo from '../components/seo'
+import ValLayout from '../components/val_layout'
+import ValTableLayout from '../components/val_table_layout'
 import {
-    farmBody,
-    backButtonDiv,
-    button,
     tableDiv,
-    table,
-    header
+    table
 } from '../components/val_layout.module.css'
 
 const ValFarm = ({ data }) => {
+    // separate farm data
+    const cropData = data.allDataJson.nodes.map(node => node.crop)
+    const feedData = data.allDataJson.nodes.map(node => node.feed)
+    const baitData = data.allDataJson.nodes.map(node => node.bait)
+
     return (
-        <>
-            <body className={farmBody}>
-                <header className={header}>Farming & Fishing Recipes</header>
-                <div className={tableDiv} style={{marginBottom: '5%'}}>
-                    <table className={table}>
-                        <thead>
-                            <tr id="tableHeader">
-                                <th></th>
-                                <th>Crop</th>
-                                <th>Biome(s) needed to grow</th>
-                                <th>Grow Time (s)</th>
-                                <th>Required Space (m)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.allDataJson.nodes.map(node =>
-                                node.crop.map((crop, index) => {
-                                let imagePath = `/images/crops/${crop.Crop.replaceAll(' ','_')}.png`;
-
-                                    return (
-                                        <tr key={index}>
-                                            <td>
-                                                <img
-                                                    src={imagePath}
-                                                    alt={crop.Crop}
-                                                />
-                                            </td>
-                                            <td>{crop.Crop}</td>
-                                            <td>{crop.Biome}</td>
-                                            <td>{crop.Growth}</td>
-                                            <td>{crop.Space}</td>
-                                        </tr>
-                                    );
-                                })
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-
-                <div className={tableDiv} style={{marginBottom: '5%'}}>
-                    <table className={table}>
-                        <thead>
-                            <tr id="tableHeader">
-                                <th></th>
-                                <th>Animal</th>
-                                <th>Feed</th>
-                                <th>Grow Time (s)</th>
-                                <th>Space Needed</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.allDataJson.nodes.map(node =>
-                                node.feed.map((feed, index) => {
-                                    let imagePath = `/images/farm/${feed.Animal.replaceAll(' ','_')}.png`;
-
-                                    return (
-                                        <tr key={index}>
-                                            <td>
-                                                <img
-                                                    src={imagePath}
-                                                    alt={feed.Animal}
-                                                />
-                                            </td>
-                                            <td>{feed.Animal}</td>
-                                            <td>{feed.Feed}</td>
-                                            <td>{feed.Growth}</td>
-                                            <td>{feed.Space}</td>
-                                        </tr>
-                                    );
-                                })
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-
-                <div className={tableDiv}>
-                    <table className={table}>
-                        <thead>
-                            <tr id="tableHeader">
-                                <th></th>
-                                <th>Bait Type</th>
-                                <th>Use</th>
-                                <th>Recipe</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.allDataJson.nodes.map(node =>
-                                node.bait.map((bait, index) => {
-                                    let imagePath = `/images/bait/${bait.Item.replaceAll(' ','_')}_fishing_bait.png`;
-
-                                    if (bait.Item === "Fishing bait") {
-                                        imagePath = `/images/bait/${bait.Item.replaceAll(' ','_')}.png`;
-                                    }
-
-                                    return (
-                                        <tr key={index}>
-                                            <td>
-                                                <img
-                                                    src={imagePath}
-                                                    alt={bait.Item}
-                                                />
-                                            </td>
-                                            <td>{bait.Item}</td>
-                                            <td>{bait.Use}</td>
-                                            <td>{bait.Recipe}</td>
-                                        </tr>
-                                    );
-                                })
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-                
-                <div className={backButtonDiv}>
-                    <button className={button} onClick={()=>{navigate("/")}}>Back to Home Page</button>
-                </div>
-            </body>
-        </>
+        <ValLayout
+            background = {`url("/images/backgrounds/ebrithil_farm.png")`}
+            title = "Farming & Fishing Recipes"
+        >
+            <ValTableLayout
+                data = {data.allDataJson.nodes}
+                headers = {["Crop", "Biome(s) needed to grow", "Grow Time (s)", "Required Space (m)"]}
+                imgBasePath = {"/images/crops"}
+                showSearch = {false}
+                contentFlag = 'crop'
+                contentNames = {["Crop", "Biome", "Growth", "Space"]}
+            />
+            <div style={{marginBottom: '5%'}}></div>
+            <ValTableLayout
+                data = {data.allDataJson.nodes}
+                headers = {["Animal", "Feed", "Grow Time (min)", "Space Needed"]}
+                imgBasePath = {"/images/animals"}
+                showSearch = {false}
+                contentFlag = 'feed'
+                contentNames = {["Animal", "Feed", "Growth", "Space"]}
+            />
+            <div style={{marginBottom: '5%'}}></div>
+            <ValTableLayout
+                data = {data.allDataJson.nodes}
+                headers = {["Bait Type", "Use", "Recipe"]}
+                imgBasePath = {"/images/bait"}
+                showSearcy = {false}
+                contentFlag = 'bait'
+                contentNames = {["Item", "Use", "Recipe"]}
+            />
+        </ValLayout>
     )
 }
 
